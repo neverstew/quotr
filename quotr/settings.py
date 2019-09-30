d54a1ad0a03f47682498dde8c1f5f0d90b7dc99a
Matthew Williams <matthew.lloyd.williams@gmail.com>
2019-09-30 16:08:24 +0100

    Login redirects to quotes list

--- quotr/settings.py
+++ quotr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,5 @@ STATICFILES_DIRS = [
 
 
 SITE_ID = 1
+
+LOGIN_REDIRECT_URL = reverse_lazy('quotes:list')
